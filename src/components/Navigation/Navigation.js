3afb7fd6de5cc919664d9f57a807a0cb1a12de2f
nurishh <91654564+nurishh@users.noreply.github.com>
2022-03-28 16:50:38 +0300

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 const Navigation = () => {
     return (
         <nav style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', margin: '150px 0 0 60px' }}>
-            <Typography variant="h5"><Link color="inherit" href="/">Главная</Link></Typography>
-            <Typography variant="h5"><Link color="inherit" href="/about">О нас</Link></Typography>
-            <Typography variant="h5"><Link color="inherit" href="/forms">Объявления</Link></Typography>
+            <Typography variant="h5"><Link to="/">Главная</Link></Typography>
+            <Typography variant="h5"><Link to="/about">О нас</Link></Typography>
+            <Typography variant="h5"><Link to="/forms">Объявления</Link></Typography>
         </nav>
     );
 };
